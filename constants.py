cf5bf5e5a41acc6ff809aab2cc458ab3c33932bc
William Wang <bn.william.w@gmail.com>
2023-10-08 12:00:15 -0400

    move directory

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
